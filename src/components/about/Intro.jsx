603fbd583a8fa26a05dc8739fef34eb8a315c168
frankamg487 <fkamgang06@gmail.com>
2025-03-04 12:04:53 +0200

    about__box color changed

--- src/components/about/Intro.jsx
+++ src/components/about/Intro.jsx
@@ -18,6 +18,11 @@ const Intro = () => {
         <h3 className="about__title">Support</h3>
         <span className="about__subtitle">En ligne 24h/24</span>
       </div>
+      <div className="about__box">
+        <i className="bx bx-support about__icon"></i>
+        <h3 className="about__title">Support</h3>
+        <span className="about__subtitle">En ligne 24h/24</span>
+      </div>
     </div>
   );
 };
