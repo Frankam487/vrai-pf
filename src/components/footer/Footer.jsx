29666dd03c943f86332dd75980fb643f53d66d16
Frankam487 <fkamgang06@gmail.com>
2025-03-07 02:29:09 +0200

    footer icon color changed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,7 @@
+import { FaFacebook, FaWhatsapp, FaGithub } from "react-icons/fa"; // Importation des icônes depuis react-icons/fa
 import "./footer.css";
-const Footer = () => {
 
+const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer__container container">
@@ -17,28 +18,25 @@ const Footer = () => {
             <a href="#testimonials" className="footer__link">Testimonial</a>
           </li>
         </ul>
-        <div className="footer__social"> 
 
-            <a target="_blank"
-          href="https://www.facebook.com/Frankam.Shopping"
-          aria-label="Facebook"
-        >
-          <i className="bx bxl-facebook"></i>
-        </a>
-        <a href="#" target="_blank" aria-label="whatsapp">
-          <i className="bx bxl-whatsapp"></i>
-        </a>
-          <a target="_blank"
-            href="https://github.com/Frankam487" aria-label="github">
-          <i className="bx bxl-twitter"></i>
-        </a>
+        <div className="footer__social">
+          <a target="_blank" href="https://www.facebook.com/Frankam.Shopping" aria-label="Facebook">
+            <FaFacebook size={30} className="social-icon facebook" />
+          </a>
+          <a href="https://api.whatsapp.com/send?phone=+237690461830&text=Salut, comment ça va ?" target="_blank" aria-label="whatsapp">
+            <FaWhatsapp size={30} className="social-icon whatsapp" />
+          </a>
+          <a target="_blank" href="https://github.com/Frankam487" aria-label="github">
+            <FaGithub size={30} className="social-icon github" />
+          </a>
         </div>
+
         <span className="footer__copy">
-          &#189 copy; Frankam. Tous droits réservés
+          &#189; copy; Frankam. Tous droits réservés
         </span>
-        
       </div>
     </footer>
   );
-}
+};
+
 export default Footer;
